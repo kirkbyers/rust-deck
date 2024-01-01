31ea5475041b03e0631506192cdacdf35533ff82
kirkbyers <kirk@cratejoy.com>
2024-01-01 15:26:17 -0600

    Init basic mechanics.

---
+++ src/deck.rs
@@ -0,0 +1,156 @@
+use rand::seq::SliceRandom;
+
+#[derive(Debug)]
+pub struct Hand {
+    cards: [Card; 2],
+}
+
+impl Hand {
+    pub fn new() -> Hand {
+        Hand {
+            cards: [Card {
+                suit: Suit::None,
+                rank: Rank::None,
+            }; 2],
+        }
+    }
+
+    pub fn fill(&mut self, deck: &mut Deck) {
+        self.cards[0] = deck.deal();
+        self.cards[1] = deck.deal();
+    }
+}
+
+#[derive(Debug)]
+pub struct Community {
+    cards: [Card; 5],
+}
+
+impl Community {
+    pub fn new() -> Community {
+        Community {
+            cards: [Card {
+                suit: Suit::None,
+                rank: Rank::None,
+            }; 5],
+        }
+    }
+
+    pub fn flop(&mut self, deck: &mut Deck) -> [Card; 3] {
+        let cards = [deck.deal(), deck.deal(), deck.deal()];
+        self.cards[0] = cards[0];
+        self.cards[1] = cards[1];
+        self.cards[2] = cards[2];
+
+        cards
+    }
+
+    pub fn turn(&mut self, deck: &mut Deck) -> Card {
+        self.cards[3] = deck.deal();
+
+        self.cards[3]
+    }
+
+    pub fn river(&mut self, deck: &mut Deck) -> Card {
+        self.cards[4] = deck.deal();
+
+        self.cards[4]
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+#[allow(dead_code)]
+pub struct Card {
+    suit: Suit,
+    rank: Rank,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[allow(dead_code)]
+enum Suit {
+    Clubs,
+    Diamonds,
+    Hearts,
+    Spades,
+    None,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[allow(dead_code)]
+enum Rank {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    None,
+}
+
+#[derive(Copy, Clone, Debug)]
+#[allow(dead_code)]
+pub struct Deck {
+    cards: [Card; 52],
+    pub dealt: usize,
+}
+
+impl Deck {
+    pub fn new() -> Deck {
+        let mut cards = [Card {
+            suit: Suit::Clubs,
+            rank: Rank::Ace,
+        }; 52];
+        let mut i = 0;
+        for suit in [Suit::Clubs, Suit::Diamonds, Suit::Hearts, Suit::Spades].iter() {
+            for rank in [
+                Rank::Ace,
+                Rank::Two,
+                Rank::Three,
+                Rank::Four,
+                Rank::Five,
+                Rank::Six,
+                Rank::Seven,
+                Rank::Eight,
+                Rank::Nine,
+                Rank::Ten,
+                Rank::Jack,
+                Rank::Queen,
+                Rank::King,
+            ]
+            .iter()
+            {
+                cards[i] = Card {
+                    suit: *suit,
+                    rank: *rank,
+                };
+                i += 1;
+            }
+        }
+        Deck { 
+            cards,
+            dealt: 0
+        }
+    }
+
+    pub fn shuffle(&mut self) {
+        self.cards.shuffle(&mut rand::thread_rng());
+        self.dealt = 0;
+    }
+
+    pub fn deal(&mut self) -> Card {
+        let card = self.cards[self.dealt];
+        self.dealt += 1;
+        card
+    }
+
+    pub fn discard(&mut self) {
+        self.dealt += 1;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+mod deck;
+
 fn main() {
-    println!("Hello, world!");
+    let mut players: [deck::Hand; 4] = [deck::Hand::new(),deck::Hand::new(),deck::Hand::new(),deck::Hand::new()];
+    let mut community = deck::Community::new();
+
+    let mut deck = deck::Deck::new();
+
+    deck.shuffle();
+    println!("deck: {:?}", deck);
+
+    for player in players.iter_mut() {
+        player.fill(&mut deck);
+        println!("player: {:?}", player)
+    }
+
+    let flop = community.flop(&mut deck);
+    println!("flop: {:?}", flop);
+
+    let turn = community.turn(&mut deck);
+    println!("turn: {:?}", turn);
+
+    let river = community.river(&mut deck);
+    println!("river: {:?}", river);
+
+    println!("community: {:?}", community);
+    println!("deck::dealt: {:?}", deck.dealt)
 }
