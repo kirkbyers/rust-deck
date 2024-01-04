168ffd8f78ec90ddb70ee96bf7827b461cc5d2f9
kirkbyers <kirk@cratejoy.com>
2024-01-03 21:07:28 -0600

    Working hand value.

--- src/deck.rs
+++ src/deck.rs
@@ -1,9 +1,10 @@
 use rand::seq::SliceRandom;
+use std::collections::HashMap;
 
 #[derive(Debug, Clone, Copy)]
 pub struct Hand {
     cards: [Card; 2],
-    pub value: (u8, u8),
+    pub value: (ScoringHands, u8),
 }
 
 impl Hand {
@@ -13,7 +14,7 @@ impl Hand {
                 suit: Suit::None,
                 rank: Rank::None,
             }; 2],
-            value: (0, 0),
+            value: (ScoringHands::None, 0),
         }
     }
 
@@ -22,95 +23,135 @@ impl Hand {
         self.cards[1] = deck.deal();
     }
 
-    pub fn hand_value(&mut self, community: &Community) -> (u8, u8) {
+    pub fn hand_value(&mut self, community: &Community) -> (ScoringHands, u8) {
         self.value = self.determine_value(community);
         self.value
     }
 
-    pub fn determine_value(&mut self, community: &Community) -> (u8, u8) {
-        let mut cards_ranked = [self.cards[0], self.cards[1], community.cards[0], community.cards[1], community.cards[2], community.cards[3], community.cards[4]];
-        cards_ranked.sort_by(|a, b| b.rank.cmp(&a.rank));
+    pub fn determine_value(&mut self, community: &Community) -> (ScoringHands, u8) {
+        let mut cards_by_rank: HashMap<Rank, Vec<Card>> = HashMap::new();
+        let mut cards_by_suit: HashMap<Suit, Vec<Card>> = HashMap::new();
 
-        let mut cards_suited = [self.cards[0], self.cards[1], community.cards[0], community.cards[1], community.cards[2], community.cards[3], community.cards[4]];
-        cards_suited.sort_by(|a, b| a.suit.cmp(&b.suit));
+        let all_cards = [self.cards[0], self.cards[1], community.cards[0], community.cards[1], community.cards[2], community.cards[3], community.cards[4]].to_vec();
 
-        // Royal Flush
-        if cards_ranked[0].rank == Rank::Ace && cards_ranked[1].rank == Rank::King && cards_ranked[2].rank == Rank::Queen && cards_ranked[3].rank == Rank::Jack && cards_ranked[4].rank == Rank::Ten {
-            return (10, Rank::Ace as u8);
+        for card in all_cards.iter() {
+            let rank = card.rank;
+            let suit = card.suit;
+
+            if cards_by_rank.contains_key(&rank) {
+                cards_by_rank.get_mut(&rank).unwrap().push(*card);
+            } else {
+                cards_by_rank.insert(rank, vec![*card]);
+            }
+
+            if cards_by_suit.contains_key(&suit) {
+                cards_by_suit.get_mut(&suit).unwrap().push(*card);
+            } else {
+                cards_by_suit.insert(suit, vec![*card]);
+            }
         }
 
-        // Straight Flush
-        for i in 0..cards_ranked.len()-5 {
-            if cards_ranked[i].rank as u8 + 1 == cards_ranked[i + 1].rank as u8 && cards_ranked[i + 1].rank as u8 + 1 == cards_ranked[i + 2].rank as u8 && cards_ranked[i + 2].rank as u8 + 1 == cards_ranked[i + 3].rank as u8 && cards_ranked[i + 3].rank as u8 + 1 == cards_ranked[i + 4].rank as u8 {
-                if cards_ranked[i].suit == cards_ranked[i + 1].suit && cards_ranked[i + 1].suit == cards_ranked[i + 2].suit && cards_ranked[i + 2].suit == cards_ranked[i + 3].suit && cards_ranked[i + 3].suit == cards_ranked[i + 4].suit {
-                    return (9, cards_ranked[i].rank as u8);
+        // Check for Royal Flush, Straight Flush, and Straight
+        for (_, cards) in cards_by_suit.iter() {
+            if cards.len() >= 5 {
+                let mut cards_ranked = cards.clone();
+                cards_ranked.sort_by(|a, b| b.rank.cmp(&a.rank));
+                cards_ranked.dedup_by(|a, b| a.rank == b.rank);
+                if cards_ranked.len() < 5 {
+                    continue;
+                }
+
+                // Check if cards_ranked contains a card with rank ten, jack, queen, king, and ace in it
+                let royal_flush_ranks = [Rank::Ten, Rank::Jack, Rank::Queen, Rank::King, Rank::Ace];
+                let card_ranks = cards_ranked.iter().map(|card| card.rank).collect::<Vec<Rank>>();
+                if royal_flush_ranks.iter().all(|rank| card_ranks.contains(&rank)) {
+                    return (ScoringHands::RoyalFlush, cards_ranked[0].rank as u8);
+                }
+
+                // Straight Flush
+                for i in 0..cards_ranked.len()-5 {
+                    if cards_ranked[i].rank as u8 + 1 == cards_ranked[i + 1].rank as u8 && cards_ranked[i + 1].rank as u8 + 1 == cards_ranked[i + 2].rank as u8 && cards_ranked[i + 2].rank as u8 + 1 == cards_ranked[i + 3].rank as u8 && cards_ranked[i + 3].rank as u8 + 1 == cards_ranked[i + 4].rank as u8 {
+                        return (ScoringHands::StraightFlush, cards_ranked[i].rank as u8);
+                    }
                 }
             }
         }
 
         // Four of a Kind
-        for i in 0..cards_ranked.len()-4 {
-            if cards_ranked[i].rank == cards_ranked[i + 1].rank && cards_ranked[i + 1].rank == cards_ranked[i + 2].rank && cards_ranked[i + 2].rank == cards_ranked[i + 3].rank {
-                return (8, cards_ranked[i].rank as u8);
+        for (rank, cards) in cards_by_rank.iter() {
+            if cards.len() == 4 {
+                return (ScoringHands::FourOfAKind, *rank as u8);
             }
         }
 
         // Full House
         let mut pair: u8 = 0;
         let mut three_of_a_kind: u8 = 0;
-        for i in 0..cards_ranked.len()-2 {
-            if i + 3 < cards_ranked.len() && cards_ranked[i].rank == cards_ranked[i + 1].rank && cards_ranked[i + 1].rank == cards_ranked[i + 2].rank  {
-                three_of_a_kind = cards_ranked[i].rank as u8;
-            } else if cards_ranked[i].rank == cards_ranked[i + 1].rank {
-                pair = cards_ranked[i].rank as u8;
+        for (rank, cards) in cards_by_rank.iter() {
+            if cards.len() == 3 && *rank as u8 > three_of_a_kind{
+                three_of_a_kind = *rank as u8;
+            }
+            if cards.len() == 2 && *rank as u8 > pair {
+                pair = *rank as u8;
             }
 
-            if pair != 0 && three_of_a_kind != 0 {
-                return (7, if pair > three_of_a_kind { pair } else { three_of_a_kind });
+            if three_of_a_kind > 0 && pair > 0 {
+                return (ScoringHands::FullHouse, three_of_a_kind);
             }
         }
 
         // Flush
-        for i in 0..cards_suited.len()-5 {
-            if cards_suited[i].suit == cards_suited[i + 1].suit && cards_suited[i + 1].suit == cards_suited[i + 2].suit && cards_suited[i + 2].suit == cards_suited[i + 3].suit && cards_suited[i + 3].suit == cards_suited[i + 4].suit {
-                return (6, cards_ranked[i].rank as u8);
+        for (_, cards) in cards_by_suit.iter() {
+            if cards.len() >= 5 {
+                let mut cards_ranked = cards.clone();
+                cards_ranked.sort_by(|a, b| b.rank.cmp(&a.rank));
+                return (ScoringHands::Flush, cards_ranked[0].rank as u8);
             }
         }
 
         // Straight
-        for i in 0..cards_ranked.len()-5 {
-            if cards_ranked[i].rank as u8 + 1 == cards_ranked[i + 1].rank as u8 && cards_ranked[i + 1].rank as u8 + 1 == cards_ranked[i + 2].rank as u8 && cards_ranked[i + 2].rank as u8 + 1 == cards_ranked[i + 3].rank as u8 && cards_ranked[i + 3].rank as u8 + 1 == cards_ranked[i + 4].rank as u8 {
-                return (5, cards_ranked[i].rank as u8);
+        let mut cards_ranked = all_cards.clone();
+        cards_ranked.sort_by(|a, b| b.rank.cmp(&a.rank));
+        cards_ranked.dedup_by(|a, b| a.rank == b.rank);
+        if cards_ranked.len() >= 5 {
+            for i in 0..cards_ranked.len()-5 {
+                if cards_ranked[i].rank as u8 + 1 == cards_ranked[i + 1].rank as u8 && cards_ranked[i + 1].rank as u8 + 1 == cards_ranked[i + 2].rank as u8 && cards_ranked[i + 2].rank as u8 + 1 == cards_ranked[i + 3].rank as u8 && cards_ranked[i + 3].rank as u8 + 1 == cards_ranked[i + 4].rank as u8 {
+                    return (ScoringHands::Straight, cards_ranked[i].rank as u8);
+                }
             }
         }
 
         // Three of a Kind
-        for i in 0..cards_ranked.len()-2 {
-            if cards_ranked[i].rank == cards_ranked[i + 1].rank && cards_ranked[i + 1].rank == cards_ranked[i + 2].rank {
-                return (4, cards_ranked[i].rank as u8);
+        let mut three_of_a_kind: u8 = 0;
+        for (rank, cards) in cards_by_rank.iter() {
+            if cards.len() == 3 && *rank as u8 > three_of_a_kind{
+                three_of_a_kind = *rank as u8;
             }
+        }
+        if three_of_a_kind > 0 {
+            return (ScoringHands::ThreeOfAKind, three_of_a_kind);
         }
 
         // One Pair
         let mut pair_rank: u8 = 0;
         let mut pair_count: u8 = 0;
-        for i in 0..cards_ranked.len()-2 {
-            if cards_ranked[i].rank == cards_ranked[i + 1].rank {
+        for (rank, cards) in cards_by_rank.iter() {
+            if cards.len() == 2 {
                 pair_count += 1;
-                if cards_ranked[i].rank as u8 > pair_rank {
-                    pair_rank = cards_ranked[i].rank as u8;
-                }
-                if pair_count == 2 {
-                    return (3, pair_rank);
+                if *rank as u8 > pair_rank {
+                    pair_rank = *rank as u8;
                 }
             }
         }
         if pair_count == 1 {
-            return (2, pair_rank);
+            return (ScoringHands::OnePair, pair_rank);
+        } else if pair_count >= 2 {
+            // Two Pair
+            return (ScoringHands::TwoPair, pair_rank);
         }
 
         // High Card
-        (0, cards_ranked[0].rank as u8)
+        (ScoringHands::HighCard, cards_ranked[0].rank as u8)
     }
 
 }
@@ -152,24 +193,25 @@ impl Community {
     }
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 #[allow(dead_code)]
 pub struct Card {
     pub suit: Suit,
     pub rank: Rank,
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, PartialOrd, Ord, Eq)]
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd, Ord, Eq, Hash)]
 pub enum Suit {
+    None,
     Clubs,
     Diamonds,
     Hearts,
     Spades,
-    None,
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, PartialOrd, Ord, Eq)]
+#[derive(Copy, Clone, Debug, PartialEq, PartialOrd, Ord, Eq, Hash)]
 pub enum Rank {
+    None,
     Two,
     Three,
     Four,
@@ -183,7 +225,21 @@ pub enum Rank {
     Queen,
     King,
     Ace,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum ScoringHands {
     None,
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    Straight,
+    Flush,
+    FullHouse,
+    FourOfAKind,
+    StraightFlush,
+    RoyalFlush,
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -249,6 +305,8 @@ impl Deck {
 
 #[cfg(test)]
 mod tests {
+    use crate::deck::ScoringHands;
+
 
     #[test]
     fn test_hand_value() {
@@ -262,7 +320,7 @@ mod tests {
                 suit: Suit::Clubs,
                 rank: Rank::King,
             }],
-            value: (0, 0),
+            value: (ScoringHands::None, 0),
         };
         let mut hand_2 = Hand {
             cards: [Card {
@@ -272,16 +330,16 @@ mod tests {
                 suit: Suit::Spades,
                 rank: Rank::Seven,
             }],
-            value: (0, 0),
+            value: (ScoringHands::None, 0),
         };
 
         let mut community = Community{
             cards: [Card {
-                suit: Suit::Clubs,
-                rank: Rank::Ace,
+                suit: Suit::Spades,
+                rank: Rank::Four,
             }, Card {
-                suit: Suit::Clubs,
-                rank: Rank::King,
+                suit: Suit::Spades,
+                rank: Rank::Five,
             }, Card {
                 suit: Suit::Clubs,
                 rank: Rank::Queen,
@@ -295,9 +353,9 @@ mod tests {
         };
 
         let hand_value = hand_1.hand_value(&community);
-        assert_eq!(hand_value, (10, Rank::Ace as u8));
+        assert_eq!(hand_value, (ScoringHands::RoyalFlush, Rank::Ace as u8));
 
-        let hand_value = hand_2.hand_value(&community);
-        assert_eq!(hand_value, (0, Rank::Seven as u8));
+        let hand_value2 = hand_2.hand_value(&community);
+        assert_eq!(hand_value2, (ScoringHands::HighCard, Rank::Queen as u8));
     }
 }
